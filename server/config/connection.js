813484d093557f96689e493ba4d3fe2c7538f302
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-06-10 11:26:19 -0700

    deleted build files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-book-search-engine'
- 
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-book-search-engine', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true, // Replace with useCreateIndexes: true
+//   useFindAndModify: false, // Replace with useFindAndModify: false
+});
 
 const db = mongoose.connection;
 
