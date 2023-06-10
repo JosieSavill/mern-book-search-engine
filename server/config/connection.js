1032775a3a489936b6a8b27c7064831b8a9ce4f0
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-06-10 11:47:51 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-book-search-engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 //   useCreateIndex: true, // Replace with useCreateIndexes: true
